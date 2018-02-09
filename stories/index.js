2973a76eac839a38f6bf8bb0ac3dd4ef539afaf2
joylty94 <wowmen1757@gmail.com>
2018-02-09 21:16:12 +0900

    LoginScreen 기본 구성 #1

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,2 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-storiesOf('button', module)
-  .add('with text', () => (
-    <button onClick={action('clicked')}>Hello button</button>
-  ))
-  .add('with some emoji', () => (
-      <button onClick={action('clicked')}>😀 😎 👍 💯</button>
-  ));
+import 'semantic-ui-css/semantic.min.css';
+import './LoginScreen';
