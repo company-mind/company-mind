61f599a9c6429d4b3b7aaea8fcab40ba66a31f80
joylty94 <wowmen1757@gmail.com>
2018-02-14 12:32:14 +0900

    #8 companyList ducks 패턴으로 구현

---
+++ src/ducks/companyList.js
@@ -0,0 +1,50 @@
+import * as firebase from 'firebase';
+
+export const LOADING = 'companyList/LOADING';
+export const SUCCESS = 'companyList/SUCCESS';
+
+export function companyListLoading() {
+  return {
+    type: LOADING,
+  };
+}
+
+export function companyListSuccess(companies) {
+  return {
+    type: SUCCESS,
+    companies,
+  };
+}
+
+const initialState = {
+  loading: false,
+  companies: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SUCCESS:
+      return {
+        loading: false,
+        companies: action.companies,
+      };
+    default:
+      return state;
+  }
+}
+
+export const fetchCompanyList = () => async (dispatch) => {
+  dispatch(companyListLoading());
+  const snapshot = await firebase.database().ref('company').once('value');
+  const companyObj = snapshot.val();
+  const companies = Object.entries(companyObj).map(([id, company]) => ({
+    ...company,
+    id,
+  }));
+  dispatch(companyListSuccess(companies));
+};
