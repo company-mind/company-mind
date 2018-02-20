105e0e63c0ff75d020c929470de15811f916d28e
joylty94 <wowmen1757@gmail.com>
2018-02-20 20:02:45 +0900

    #8 로딩 디자인 변경

--- src/hocs/withLoading.js
+++ src/hocs/withLoading.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Grid, Icon } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const FullHeightGrid = styled(Grid) `
+  height: 100vh;
+`;
 
 export default function withLoading(WrappedComponent) {
   return class extends Component {
     render() {
       if (this.props.loading) {
         return (
-          <Dimmer active inverted>
-            <Loader />Loading
-          </Dimmer>
+          <FullHeightGrid centered verticalAlign="middle">
+            <Grid.Column style={{ width: '100px' }}>
+              <Icon loading name='spinner' size='huge' color='black' />
+            </Grid.Column>
+          </FullHeightGrid>
         );
       }
       return (
