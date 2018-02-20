efb48881c2808c61bf85cbd753580db30136b2f2
joylty94 <wowmen1757@gmail.com>
2018-02-20 21:55:52 +0900

    #8 UI 변경

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Icon, Segment } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const NewColumn = styled(Grid.Column)`
+  padding: 0px !important;
+`
+
+const MSegment = styled(Segment)`
+  margin: 0px !important;
+`
+const NewDiv = styled.div`
+  padding : 2px;
+`
+const NewUl = styled.ul`
+  list-style: none;
+  display: flex;
+  padding : 0px;
+  margin : 0px;
+  margin-left: 4px;
+`
 
 export default class CompanyList extends Component {
   static defaultProps = {
@@ -9,29 +28,30 @@ export default class CompanyList extends Component {
   render() {
     const { companies } = this.props;
     return (
-      <Grid columns={3}>
+      <Segment>
         {
           companies.map(({
         id, name, group, address, scrapScore, reviewScore, emotionScore,
          }) => (
-              <Grid.Row stretched>
-                <Grid.Column>
-                  <div>{name}</div>
-                  <div>{address}</div>
-                  <div>{scrapScore}</div>
-                </Grid.Column>
-                <Grid.Column>
-                  <div>{group}</div>
-                  <div></div>
-                  <div>{reviewScore}</div>
-                </Grid.Column>
-                <Grid.Column>
-                  <div>{emotionScore}</div>
-                </Grid.Column>
-              </Grid.Row>
+        <MSegment key={id}>
+          <Grid>
+            <Grid.Row stretched>
+              <NewColumn width={10} style={{marginLeft: '25px'}}>
+                <NewDiv style={{ fontFamily: 'Spoqa-Han-Sans-Bold', fontSize: '1.2rem'}}>{name}</NewDiv>
+                <NewUl>
+                  <li><NewDiv>{address}</NewDiv><NewDiv><Icon name='pencil' size='large' />{reviewScore}</NewDiv></li>
+                  <li style={{marginLeft:'20px', textAlign:'center'}}><NewDiv>{group}</NewDiv><NewDiv><Icon name='empty star' size='large' />{scrapScore}</NewDiv></li>
+                </NewUl>
+              </NewColumn>
+              <NewColumn width={3}>
+                <NewDiv>{emotionScore}</NewDiv>
+              </NewColumn>
+            </Grid.Row>
+          </Grid>
+        </MSegment>
          ))
         }
-      </Grid>
+      </Segment>
     )
   }
 }
