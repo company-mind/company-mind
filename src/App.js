498b7f8f285c29f13c9600c8edc6af46c396b4ce
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-02-21 18:38:06 +0900

    라우팅 설정 수정
    1) /nickname을 LoginScreenContainer로 수정
    2) 임시로 /, /main 라우팅을 설정해둠

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import LoginScreenContainer from './containers/LoginScreenContainer';
-import NicknameScreen from './components/NicknameScreen';
+import withAuth from '../src/hocs/withAuth';
+
+const Home = () => <Redirect to="/main" />;
+const Main = () => <div>Main</div>;
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" exact />
+          <Route path="/" exact component={withAuth(Home)} />
           <Route path="/login" component={LoginScreenContainer} />
-          <Route path="/nickname" component={NicknameScreen} />
+          <Route path="/nickname" component={LoginScreenContainer} />
+          <Route path="/main" component={withAuth(Main)} />
         </div>
       </BrowserRouter>
     );
