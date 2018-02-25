b18d1f202df3fa28477d30c36c993b965ffdc9f7
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-02-26 05:25:39 +0900

    review ducks 작성

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import companyList from './companyList';
+import review from './review';
 
 export default combineReducers({
   companyList,
+  review,
 });
 
