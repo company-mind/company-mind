34be37b86f6546d5c9f389a176f0f4e5aa092ec6
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-02-27 19:49:29 +0900

    Redux Devtool 사용 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './ducks';
@@ -12,7 +12,11 @@ import CompanyListContainer from './containers/CompanyListContainer';
 import CompanyDetailContainer from './containers/CompanyDetailContainer';
 import ReviewFormContainer from './containers/ReviewFormContainer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 class App extends Component {
   // router 설정
