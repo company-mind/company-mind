eee6bcfe6ab355520d28b323439f79f0a5eecfc1
joylty94 <wowmen1757@gmail.com>
2018-03-01 20:41:48 +0900

    #9 CompanyArticleContainer 구현

---
+++ src/containers/CompanyArticleContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CompanyArticle from '../components/CompanyArticle';
+import { dispatchCompanyArticle } from '../ducks/companyArticle';
+
+class CompanyArticleContainer extends Component {
+  static defaultProps = {
+    onMount: () => { },
+    companyItem: [],
+  }
+
+  componentDidMount() {
+    this.props.onMount(this.props.match.params)
+  }
+
+  render(){
+    const { onMount, ...rest } = this.props
+    return (
+      <CompanyArticle {...rest} />
+    )
+  }
+}
+
+export default connect (
+  // mapStateToProps
+  state => ({
+    companyItem: state.companyArticle.companyItem
+  }),
+  // mapDispatchToProps
+  dispatch => ({
+    onMount: ({companyId}) => {
+      dispatch(dispatchCompanyArticle({ companyId }))
+    }
+  }),
+)(CompanyArticleContainer)
