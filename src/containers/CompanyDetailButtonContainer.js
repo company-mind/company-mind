20c21aeb7d21f4d41969d49a1b4a749476385988
joylty94 <wowmen1757@gmail.com>
2018-03-04 16:56:34 +0900

    #9 CompanyDetailButtonContainer 생성

--- src/containers/CompanyDetailButtonContainer.js
+++ src/containers/CompanyDetailButtonContainer.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CompanyDetailButton from '../components/CompanyDetailButton';
+import CompanyDetailOnButton from '../components/CompanyDetailOnButton';
 
-export default class CompanyDetailButtonContainer extends Component{
+import { dispatchCompanyDetailButton } from '../ducks/companyDetailButton';
+
+
+
+class CompanyDetailButtonContainer extends Component{
+  static defaultProps = {
+    onMount: () => { },
+    scrap: false,
+  }
+
+  componentDidMount() {
+    this.props.onMount(this.props.match.params)
+  }
+
   render() {
-    const { ...rest } = this.props;
-    return(
-      <CompanyDetailButton {...rest} />
-    )
+    const { onMount, scrap, ...rest } = this.props;
+    console.log(rest)
+    if(scrap){
+      return(
+        <CompanyDetailOnButton {...rest}/>
+      )
+    } else {
+      return(
+        <CompanyDetailButton {...rest} />
+      )
+    }
   }
 }
+
+export default connect(
+  // mapStateToProps
+  state => ({
+    scrap: state.companyDetailButton.scrap,
+  }),
+  // mapDispatchToProps
+  dispatch => ({
+    onMount: ({ companyId }) => {
+      dispatch(dispatchCompanyDetailButton({ companyId }))
+    }
+  }),
+)(CompanyDetailButtonContainer)
