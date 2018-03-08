a7a31e1e3d19be85e3d83639127b3b223e969a65
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-03-09 05:06:46 +0900

    moment.js 추가, locale 설정, relative 시간 설정

--- src/components/CompanyReviewList.js
+++ src/components/CompanyReviewList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Grid, Icon, Segment, Pagination, Button } from 'semantic-ui-react';
 import Dock from 'react-dock';
+import moment from 'moment';
+import 'moment/locale/ko';
+
+moment.locale('ko');
 
 const NewColumn = styled(Grid.Column)`
   padding: 0px !important;
@@ -113,7 +117,7 @@ export default class CompanyReviewList extends Component {
                 </NewRow>
                 <NewRow>
                   <NewColumn>
-                    <div style={{ paddingRight: '6px', marginLeft: '14px' }}>{time}</div>
+                    <div style={{ paddingRight: '6px', marginLeft: '14px' }}>{moment(time).fromNow()}</div>
                   </NewColumn>
                 </NewRow>
                 <Segment style={{ width: '100%', margin: '0', padding: '0 14px' }}>
