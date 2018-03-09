3e08f7f033c72ae63e1ea6ed99641cf85c0d0ba0
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-03-09 13:05:34 +0900

    카멜케이스였던 url들을 소문자로 변경

--- src/components/CompanyDetailButton.js
+++ src/components/CompanyDetailButton.js
@@ -25,7 +25,7 @@ export default class CompanyDetailButton extends Component {
       <MSegment>
         <Grid columns={2}>
           <NewColumn textAlign="center">
-            <NewLink to={`/companyDetail/${companyId}/addReview`}>
+            <NewLink to={`/companydetail/${companyId}/addreview`}>
               <Segment>
                 <Icon name="pencil" size="large" />리뷰 쓰기
               </Segment>
