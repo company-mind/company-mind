9aa9a00ae3b531597d0fc8009f1adafb68ec1d40
joylty94 <wowmen1757@gmail.com>
2018-04-18 21:14:46 +0900

    CompanyList min-height 설정

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
-import { Grid, Icon, Segment, Pagination, Search, Input } from 'semantic-ui-react';
+import { Grid, Icon, Segment, Pagination, Search } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const NewColumn = styled(Grid.Column)`
   padding: 0px !important;
   margin-left: 8px;
-`
+`;
 const NewLColumn = styled(Grid.Column)`
   padding: 0px !important;
   margin-left: 25px;
-`
-
+`;
 const MSegment = styled(Segment)`
   margin: 0px !important;
   cursor: pointer;
-`
+`;
 const NewDiv = styled.div`
   padding : 2px;
-`
+`;
 const NameDiv = styled.div`
   padding : 2px;
   font-family: 'Spoqa-Han-Sans-Bold';
-`
+`;
 const Emoge = styled.div`
   font-Size: 55px;
   text-Align: center;
-`
+`;
 const NewLink = styled(Link)`
   color: inherit;
   text-decoration: none;
-`
-const NewRow = styled(Grid.Row) `
+`;
+const MinDiv = styled.div`
+  min-height: 49rem;
+`;
+const NewRow = styled(Grid.Row)`
   padding: 0px !important;
   margin-top: 10px;
-`
-const NewSearch = styled(Search)`
-  width: 100%;
-`
+`;
 
 export default class CompanyList extends Component {
   static defaultProps = {
@@ -45,67 +44,71 @@ export default class CompanyList extends Component {
     onMount: () => { },
   }
   handlePaginationChange = (e, { activePage }) => {
-    this.props.onPaginationChange({activePage})
+    this.props.onPaginationChange({ activePage });
   }
   handleResultSelect = (e, { result }) => {
-    this.props.onResultSelect({ result })
+    this.props.onResultSelect({ result });
   }
   handleSearchChange = (e, { value }) => {
-    this.props.onSearchChange({ value })
+    this.props.onSearchChange({ value });
   }
   render() {
-    const { pageItems, pageNumber, resultRenderer, searchLoading, value, results } = this.props;
+    const {
+      pageItems, pageNumber, resultRenderer, searchLoading, value, results,
+    } = this.props;
     return (
       <MSegment>
-        <Search
-          style={{ marginBottom: "15px", marginLeft: "40px"}}
-          loading={searchLoading}
-          onResultSelect={this.handleResultSelect}
-          onSearchChange={this.handleSearchChange}
-          results={results}
-          value={value}
-          resultRenderer={resultRenderer}
-          size="huge"
-        />
-        {
+        <MinDiv>
+          <Search
+            style={{ marginBottom: '15px', marginLeft: '40px' }}
+            loading={searchLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={this.handleSearchChange}
+            results={results}
+            value={value}
+            resultRenderer={resultRenderer}
+            size="huge"
+          />
+          {
           pageItems.map(({
         id, name, group, address, scrapScore, reviewScore, emotionScore, itemProps = {},
          }) => (
-        <NewLink to={`/companydetail/${id}`} key={id}>
-          <MSegment {...itemProps} >
-            <Grid>
-              <Grid.Row stretched>
-                <NewLColumn width={10}>
-                  <NameDiv style={{ fontSize: '1.4rem', marginLeft: '10px'}}>{name}</NameDiv>
-                    <Grid>
-                      <Grid.Row>
-                        <NewLColumn width={7}>
-                        <NewDiv>{address}</NewDiv>
-                        </NewLColumn>
-                        <NewColumn width={7}>
-                        <NewDiv>{group}</NewDiv>
-                        </NewColumn>
-                        <NewLColumn width={7}>
-                        <NewDiv><Icon name='pencil' size='large' />{reviewScore}</NewDiv>
-                        </NewLColumn>
-                        <NewColumn width={7}>
-                        <NewDiv><Icon name='empty star' size='large' />{scrapScore}</NewDiv>
-                        </NewColumn>
-                      </Grid.Row>
-                    </Grid>
-                </NewLColumn>
-                <NewLColumn style={{ marginLeft: '-2px' }} width={3}>
-                  <Emoge>{emotionScore}</Emoge>
-                </NewLColumn>
-              </Grid.Row>
-            </Grid>
-          </MSegment>
-        </NewLink>
+           <NewLink to={`/companydetail/${id}`} key={id}>
+             <MSegment {...itemProps} >
+               <Grid>
+                 <Grid.Row stretched>
+                   <NewLColumn width={10}>
+                     <NameDiv style={{ fontSize: '1.4rem', marginLeft: '10px' }}>{name}</NameDiv>
+                     <Grid>
+                       <Grid.Row>
+                         <NewLColumn width={7}>
+                           <NewDiv>{address}</NewDiv>
+                         </NewLColumn>
+                         <NewColumn width={7}>
+                           <NewDiv>{group}</NewDiv>
+                         </NewColumn>
+                         <NewLColumn width={7}>
+                           <NewDiv><Icon name="pencil" size="large" />{reviewScore}</NewDiv>
+                         </NewLColumn>
+                         <NewColumn width={7}>
+                           <NewDiv><Icon name="empty star" size="large" />{scrapScore}</NewDiv>
+                         </NewColumn>
+                       </Grid.Row>
+                     </Grid>
+                   </NewLColumn>
+                   <NewLColumn style={{ marginLeft: '-2px' }} width={3}>
+                     <Emoge>{emotionScore}</Emoge>
+                   </NewLColumn>
+                 </Grid.Row>
+               </Grid>
+             </MSegment>
+           </NewLink>
          ))
         }
+        </MinDiv>
         <Grid>
           <NewRow>
-            <NewColumn textAlign='center'>
+            <NewColumn textAlign="center">
               <Pagination
                 defaultActivePage={1}
                 firstItem={null}
@@ -119,6 +122,6 @@ export default class CompanyList extends Component {
           </NewRow>
         </Grid>
       </MSegment>
-    )
+    );
   }
 }
