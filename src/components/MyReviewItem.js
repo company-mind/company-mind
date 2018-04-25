60d7333cd8acbe78c030d013e35ab8086ee3293f
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-04-25 14:38:21 +0900

    스타일링: MyReviewItem CSS Grid로 스타일링 변경

--- src/components/MyReviewItem.js
+++ src/components/MyReviewItem.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import Emotion from './Emotion';
 
-const ReviewGrid = styled(Grid)`
-  display: flex !important;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 20px !important;
-  margin-left: 15px !important;
-  height: 90px;
+const ReviewGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  height: 100px;
+  margin-left: 9px;
 `;
 
 const ReviewHeader = styled.header`
-  width: 28%;
+  grid-area: 1 / 1 / 3 / 4;
   font-size: 1.2rem;
   font-weight: 700;
   white-space: nowrap;
@@ -24,31 +22,64 @@ const ReviewHeader = styled.header`
   overflow: hidden;
 `;
 
+const ReviewHeaderSpan = styled.span`
+  font-size: 0.9rem;
+  color: #495057;
+  padding-left: 2px;
+`;
+
+const ReviewTextHeader = styled.div`
+  font-size: 0.7rem;
+  font-weight: 700;
+  padding-top: 8px;
+
+  grid-area: 2 / 1 / 3 / 3;
+`;
+
 const ReviewText = styled.p`
+  margin-top: 4px;
+  grid-area: 3 / 1 / 5 / 4;
   color: #495057;
   font-size: 0.9rem;
   word-wrap: break-word;
   word-break: break-all;
   text-overflow: ellipsis;
   overflow: hidden;
-  width: 45%;
-  margin-left: -10px;
+  border-radius: 6px;
+  background: #efefef;
+  padding: 4px;
+`;
+
+const EmojiScoreHeader = styled.span`
+  font-size: 0.7rem;
+  display: block;
+  font-weight: 700;
+  grid-area: 1/4/5/5;
+  margin: 0 auto;
+  padding-top: 8px;
+  text-align: center;
 `;
 
 const EmojiScore = styled.div`
+  grid-area: 1 / 4 / 5 / 5;
   font-size: 64px;
-  margin-top: -18px;
+  margin-top: 10px;
+  text-align: center;
 `;
 
 const MyReviewItem = props => (
   <ReviewGrid>
-    <Grid.Row>
-      <ReviewHeader>{props.review.companyName}</ReviewHeader>
-      <ReviewText>{props.review.content}</ReviewText>
-      <EmojiScore>
-        <Emotion score={props.review.emotion} />
-      </EmojiScore>
-    </Grid.Row>
+    {console.log(props)}
+    <ReviewHeader>
+      {props.review.companyName}
+      <ReviewHeaderSpan>에 리뷰를 남겼습니다.</ReviewHeaderSpan>
+    </ReviewHeader>
+    <ReviewTextHeader>텍스트 리뷰🖋</ReviewTextHeader>
+    <ReviewText>{props.review.content}</ReviewText>
+    <EmojiScoreHeader>감정점수 {props.review.emotion}점!</EmojiScoreHeader>
+    <EmojiScore>
+      <Emotion score={props.review.emotion} />
+    </EmojiScore>
   </ReviewGrid>
 );
 
