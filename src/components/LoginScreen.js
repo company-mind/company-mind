b10614bb1cc34e2250d0098146ceb8cd20723fe0
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-04-26 15:12:12 +0900

    스타일링: 로그인 스크린 스타일 변경 및 배경 이미지 추가

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import { Button, Header, Icon, Grid, Segment } from 'semantic-ui-react';
 import styled from 'styled-components';
 
+import { city } from '../assets/images';
+
 const FullHeightGrid = styled(Grid)`
-  height: 100vh;
+  height: 102vh !important;
+  background: papayawhip url(${city}) 100% no-repeat;
+`;
+
+const MainHeader = styled(Header)`
+  font-size: 64px !important;
+  font-weight: 900 !important;
+  color: #fff !important;
+  text-shadow: 1px 1px 8px rgba(10, 44, 78, 0.6);
+`;
+
+const WelcomeHeader = styled(Header)`
+  color: #fff !important;
+  font-size: 36px !important;
+  text-shadow: 1px 1px 20px rgba(10, 44, 78, 0.8);
+`;
+
+const LoginHeader = styled(Header)`
+  color: #fff !important;
+  font-size: 22px !important;
+  margin-top: -8px !important;
+  text-shadow: 1px 1px 8px rgba(10, 44, 78, 0.6);
 `;
 
 export default class LoginScreen extends Component {
@@ -16,14 +39,17 @@ export default class LoginScreen extends Component {
   render() {
     return (
       <FullHeightGrid centered verticalAlign="middle">
-        <Grid.Column style={{ width: '280px' }}>
-          <Header as="h2" textAlign="center">
+        <Grid.Column style={{ width: '300px' }}>
+          <MainHeader as="h1" textAlign="center" size="huge">
+            Company Mind
+          </MainHeader>
+          <WelcomeHeader as="h2" textAlign="center">
             반갑습니다!
-          </Header>
-          <Header as="h4" textAlign="center">
-            로그인 해주세요
-          </Header>
-          <Segment stacked textAlign="center" padded="very">
+          </WelcomeHeader>
+          <LoginHeader as="h3" textAlign="center">
+            먼저 로그인 해주세요
+          </LoginHeader>
+          <Segment textAlign="center" padded>
             <Button color="google plus" fluid onClick={this.props.onGoogleLogin}>
               <Icon name="google plus" /> 구글로 로그인
             </Button>
@@ -41,7 +67,7 @@ export default class LoginScreen extends Component {
               onClick={this.props.onFaceBookLogin}
               style={{ marginTop: '10px' }}
             >
-              <Icon name="facebook" /> 구글로 로그인
+              <Icon name="facebook" /> 페이스북으로 로그인
             </Button>
           </Segment>
         </Grid.Column>
